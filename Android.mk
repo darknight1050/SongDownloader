413814fa66683710257fa04c7f5626d8679418ac
darknight1050 <darkdeveloping1050@gmail.com>
2021-07-05 19:23:38 +0200

    Update qpm libs

--- Android.mk
+++ Android.mk
@@ -18,17 +18,17 @@ TARGET_ARCH_ABI := $(APP_ABI)
 
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
-# Creating prebuilt for dependency: codegen - version: 0.9.0
+# Creating prebuilt for dependency: codegen - version: 0.10.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_9_0
+LOCAL_MODULE := codegen_0_10_2
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_9_0.so
+LOCAL_SRC_FILES := extern/libcodegen_0_10_2.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.5
+# Creating prebuilt for dependency: beatsaber-hook - version: 2.0.2
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_3_5
+LOCAL_MODULE := beatsaber-hook_2_0_2
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_5.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_0_2.so
 LOCAL_CPP_FEATURES += exceptions 
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.1.0
@@ -37,19 +37,19 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.11.0
+# Creating prebuilt for dependency: custom-types - version: 0.12.2
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.8.0
+# Creating prebuilt for dependency: questui - version: 0.9.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
 LOCAL_SRC_FILES := extern/libquestui.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: songloader - version: 0.4.0
+# Creating prebuilt for dependency: songloader - version: 0.5.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := songloader
 LOCAL_EXPORT_C_INCLUDES := extern/songloader
@@ -77,8 +77,8 @@ LOCAL_SRC_FILES += $(call rwildcard,src/**,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_5
-LOCAL_SHARED_LIBRARIES += codegen_0_9_0
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_0_2
+LOCAL_SHARED_LIBRARIES += codegen_0_10_2
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += songloader
