5d6f9e22678c04b95434b95b66e226bb70c464f1
EnderdracheLP <30808481+EnderdracheLP@users.noreply.github.com>
2021-08-03 17:14:30 +0200

    Remove old BeatSaver values

--- src/Types/BeatmapsIO/Page.cpp
+++ src/Types/BeatmapsIO/Page.cpp
@@ -2,8 +2,4 @@
 
 DESERIALIZE_METHOD(BeatmapsIO, Page,
     DESERIALIZE_VECTOR(Docs, docs, Beatmap)
-    //DESERIALIZE_VALUE(TotalDocs, totalDocs, Int)
-    //DESERIALIZE_VALUE(LastPage, lastPage, Int)
-    //DESERIALIZE_VALUE_OPTIONAL(PreviousPage, previousPage, Int)
-    //DESERIALIZE_VALUE_OPTIONAL(NextPage, nextPage, Int)
 )
