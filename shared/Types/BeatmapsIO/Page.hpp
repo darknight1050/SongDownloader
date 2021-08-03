7954efa9f28e2754537dae1f687bc52da571871c
EnderdracheLP <30808481+EnderdracheLP@users.noreply.github.com>
2021-08-03 17:13:55 +0200

    Remove old BeatSaver values

--- shared/Types/BeatmapsIO/Page.hpp
+++ shared/Types/BeatmapsIO/Page.hpp
@@ -6,8 +6,4 @@
 
 DECLARE_JSON_CLASS(BeatmapsIO, Page,
     GETTER_VALUE(std::vector<BeatmapsIO::Beatmap>, Docs);
-    //GETTER_VALUE(int, TotalDocs);
-    //GETTER_VALUE(int, LastPage);
-    //GETTER_VALUE_OPTIONAL(int, PreviousPage);
-    //GETTER_VALUE_OPTIONAL(int, NextPage);
 )
