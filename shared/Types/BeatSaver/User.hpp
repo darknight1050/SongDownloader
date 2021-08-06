714efd6b4f7e7080c708b0af99d645e0f6b9358e
EnderdracheLP <enderdrachelp@gmail.com>
2021-08-07 00:31:26 +0200

    Removed optional testplay bool, somehow causes crashes @darknight1050 should maybe look into this

--- shared/Types/BeatSaver/User.hpp
+++ shared/Types/BeatSaver/User.hpp
@@ -5,9 +5,8 @@
 DECLARE_JSON_CLASS(BeatSaver, UserDetail, 
     GETTER_VALUE(int, Id);
     GETTER_VALUE(std::string, Username);
-    GETTER_VALUE(std::string, AvatarURL);
     GETTER_VALUE_OPTIONAL(std::string, Hash);
-    //GETTER_VALUE(BeatSaver::UserStats, stats); // Listed in documentation as well as testplay not seen in any recent requests though
-    GETTER_VALUE_OPTIONAL(bool, Testplay);
+    GETTER_VALUE(std::string, AvatarURL);
+    //GETTER_VALUE_OPTIONAL(bool, Testplay);
     GETTER_VALUE_OPTIONAL(BeatSaver::UserStats, Stats);
 )
