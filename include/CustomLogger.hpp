45e0fe767d6b2e4a251792ec575553225efc7e2b
darknight1050 <darkdeveloping1050@gmail.com>
2021-08-18 17:57:36 +0200

    Disable debug log

--- include/CustomLogger.hpp
+++ include/CustomLogger.hpp
@@ -2,8 +2,8 @@
 
 //#define LOG_INFO(value...)
 #define LOG_INFO(value...) getLogger().info(value) 
-//#define LOG_DEBUG(value...) 
-#define LOG_DEBUG(value...) getLogger().debug(value) 
+#define LOG_DEBUG(value...) 
+//#define LOG_DEBUG(value...) getLogger().debug(value) 
 //#define LOG_ERROR(value...)
 #define LOG_ERROR(value...) getLogger().error(value) 
 
