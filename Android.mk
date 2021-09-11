6f2d173a4720d94c9c55a91c583b9ea047282387
darknight1050 <darkdeveloping1050@gmail.com>
2021-09-11 17:54:55 +0200

    Update to 1.17.1

--- Android.mk
+++ Android.mk
@@ -23,13 +23,13 @@ LOCAL_MODULE := zip
 LOCAL_EXPORT_C_INCLUDES := include/zip/src
 LOCAL_SRC_FILES := $(call rwildcard,include/zip/src/,*.c)
 include $(BUILD_STATIC_LIBRARY)
-# Creating prebuilt for dependency: songloader - version: 0.7.0
+# Creating prebuilt for dependency: songloader - version: 0.7.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := songloader
 LOCAL_EXPORT_C_INCLUDES := extern/songloader
 LOCAL_SRC_FILES := extern/libsongloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.11.0
+# Creating prebuilt for dependency: questui - version: 0.11.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
@@ -47,12 +47,6 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.13.0
-include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_13_0
-LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_13_0.so
-include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: custom-types - version: 0.12.7
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
@@ -65,6 +59,12 @@ LOCAL_MODULE := curl
 LOCAL_EXPORT_C_INCLUDES := extern/libcurl
 LOCAL_SRC_FILES := extern/libcurl.a
 include $(PREBUILT_STATIC_LIBRARY)
+# Creating prebuilt for dependency: codegen - version: 0.14.0
+include $(CLEAR_VARS)
+LOCAL_MODULE := codegen
+LOCAL_EXPORT_C_INCLUDES := extern/codegen
+LOCAL_SRC_FILES := extern/libcodegen.so
+include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS) 
 LOCAL_MODULE := songdownloader
@@ -75,8 +75,8 @@ LOCAL_SHARED_LIBRARIES += songloader
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += codegen_0_13_0
 LOCAL_SHARED_LIBRARIES += custom-types
+LOCAL_SHARED_LIBRARIES += codegen
 LOCAL_STATIC_LIBRARIES += zip
 LOCAL_STATIC_LIBRARIES += curl
 LOCAL_LDLIBS += -llog -lz
