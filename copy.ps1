9570e96488485f760eaed7c8ad591fee2bb3882f
darknight1050 <darkdeveloping1050@gmail.com>
2022-01-31 14:29:04 +0100

    Fix build scripts

--- copy.ps1
+++ copy.ps1
@@ -1,8 +1,19 @@
-& $PSScriptRoot/build.ps1
-if ($?) {
-    adb push build/libsongdownloader.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libsongdownloader.so
-    if ($?) {
-        adb shell am force-stop com.beatgames.beatsaber
-        adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
-    }
+param (
+    [Parameter(Mandatory=$false)]
+    [Switch]$useDebug,
+    [Parameter(Mandatory=$false)]
+    [Switch]$log
+)
+
+& ./build.ps1
+if ($useDebug.IsPresent) {
+    & adb push build/debug/libsongdownloader.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libsongdownloader.so
+} else {
+    & adb push build/libsongdownloader.so /sdcard/Android/data/com.beatgames.beatsaber/files/mods/libsongdownloader.so
+}
+
+& adb shell am force-stop com.beatgames.beatsaber
+& adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
+if ($log.IsPresent) {
+    & ./log.ps1
 }
